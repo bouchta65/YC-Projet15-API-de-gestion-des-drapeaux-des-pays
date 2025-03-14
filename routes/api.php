b5dc6fd20f9022bdd49bb15fa756957f087b84e4
bouchta65 <bouchtamohamed01@gmail.com>
2025-03-14 22:12:49 +0000

    add autentification route

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\api\PaysController;
+use App\Http\Controllers\api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
 
 Route::get('/user', function (Request $request) {
@@ -19,5 +19,10 @@ Route::delete('pays/delete/{id}',[PaysController::class,'delete']);
 
 Route::post('pays/update/{id}/flag', [PaysController::class, 'updateFlag']);
 Route::get('pays/update/{id}/flag', [PaysController::class, 'showImages']);
+
+Route::post('register',[AuthController::class,'register']);
+Route::post('login',[AuthController::class,'login']);
+Route::post('/logout', [AuthController::class,"logout"])->middleware("auth:sanctum");
+
 
 // Route::post('register',[AuthController::class,'register']);
