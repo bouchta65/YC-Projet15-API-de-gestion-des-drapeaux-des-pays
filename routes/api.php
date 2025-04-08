a9adc8168ece5bafc5045d90b52814a899cfcb09
Bouchta_Mohamed <bouchtamohamed01@gmail.com>
2025-04-08 12:00:31 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -10,15 +10,19 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
+Route::middleware(['auth:sanctum')->group(function () {
 Route::get('pays',[PaysController::class,'index']);
 Route::post('pays/save',[PaysController::class,'store']);
 Route::put('pays/update/{id}',[PaysController::class,'update']);
 Route::delete('pays/delete/{id}',[PaysController::class,'delete']);
-
-
 Route::post('pays/update/{id}/flag', [PaysController::class, 'updateFlag']);
 Route::get('pays/update/{id}/flag', [PaysController::class, 'showImages']);
+});
+
+
+
+
+
 
 Route::post('register',[AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
